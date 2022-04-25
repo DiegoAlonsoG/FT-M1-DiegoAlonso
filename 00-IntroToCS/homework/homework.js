6268c83d42071723e2bb3399736770924d3997fc
Seek <seek@iMac-de-Seek.local>
2022-04-25 14:58:26 -0500

    dia 1

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -3,10 +3,21 @@
 function BinarioADecimal(num) {
   // tu codigo aca
   // algo
+  var suma = 0;
+  for (var i=num.length-1; i>=0; i--) {
+      suma = suma + num[i]*Math.pow(2,num.length-1-i)
+  }
+  return suma
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var aux = []
+  while (num > 0) {
+    aux.unshift(num % 2)
+    num = Math.floor(num / 2)
+  }
+  return aux.join("")
 }
 
 // No se pueden usar: 
